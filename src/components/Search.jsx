0a1f17b49b188516fc112ea2c8429cf209e2944e
Mirko Schaefer <codewithmirko@gmail.com>
2024-04-09 15:45:25 +0200

    solved lab Mirko Schaefer

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import Axios from "axios";
+
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
+
 function Search() {
+  const [query, setQuery] = useState("");
+  const [result, setResult] = useState([]);
+
+  const handleInput = (e) => {
+    const inputValue = e.target.value;
+    setQuery(inputValue);
+    console.log(inputValue);
+
+    Axios.get(`${API_URL}/search?q=${inputValue}`)
+      .then((response) => {
+        console.log(response);
+        setResult(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="d-inline-flex justify-content-center align-items-center w-100 p-4">
       <div className="input-group mb-2 w-50">
@@ -10,8 +32,15 @@ function Search() {
         <input
           type="text"
           className="form-control search-bar"
+          onChange={handleInput}
+          value={query}
         />
       </div>
+      <ul>
+        {result.map((beer) => (
+          <li key={beer.id}>{beer.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/pages/RandomBeerPage.jsx
+++ src/pages/RandomBeerPage.jsx
@@ -1,23 +1,27 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import beersJSON from "./../assets/beers.json";
+import { useEffect } from "react";
+import Axios from "axios";
 
+const API_URL = "https://ih-beers-api2.herokuapp.com/beers";
 
 function RandomBeersPage() {
   // Mock initial state, to be replaced by data from the Beers API. Store the beer info retrieved from the Beers API in this state variable.
-  const [randomBeer, setRandomBeer] = useState(beersJSON[0]);
+  const [randomBeer, setRandomBeer] = useState();
 
   // React Router hook for navigation. We use it for the back button. You can leave this as it is.
   const navigate = useNavigate();
 
+  const fetchRandomBeer = () => {
+    Axios.get(`${API_URL}/random`)
+      .then((response) => setRandomBeer(response.data))
+      .catch((error) => console.log(error));
+  };
 
-  
-  // TASKS:
-  // 1. Set up an effect hook to make a request for a random beer from the Beers API.
-  // 2. Use axios to make a HTTP request.
-  // 3. Use the response data from the Beers API to update the state variable.
-
-
+  useEffect(() => {
+    fetchRandomBeer();
+  }, []);
 
   // The logic and the structure for the page showing the random beer. You can leave this as it is.
   return (
